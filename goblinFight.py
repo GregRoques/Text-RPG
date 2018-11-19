3a4468d142fc65186f93d35e17209c385d4ebe1c
Greg Roques <gregroques@Gregs-MacBook-Pro.local>
2018-11-19 10:22:37 -0500

    Second update

--- goblinFight.py
+++ goblinFight.py
@@ -7,6 +7,7 @@
 
 # Get the hero name from the player
 hero_name= raw_input("What is your name brave adventurer? ")
+print "Welcome, %s! Thou art brave!" % hero_name
 
 def fight():
     #declare some variables
@@ -19,8 +20,36 @@ def fight():
     #run the game as long as both characters have health > 0
 
     while (hero_health > 0 and goblin_health > 0):
-        
+        print""
+        print """You have %d health and %d power. 
+The goblin has %d health and %d power. 
+What do you want to do?
+1. fight goblin
+2. dance
+3. flee
+> """ % (hero_health, hero_power, goblin_health, goblin_power)
 
+        userInput = raw_input()
 
+        if (userInput == "1"):
+            # The hero has decided to attack
+            # subtract goblins health by hero power
+            goblin_health -= hero_power
+            print""
+            print "You have done %d damage to the goblin." % hero_power 
+        elif (userInput == "2"):
+            hero_health += 10
+            print""
+            print """The goblin stares at you in disbelief of your stupidity.
+Little does he know you just gained 10 health. His jaw drops as your wounds heal."""
+            print "Your health is now %d." % hero_health
+        elif (userInput == "3"):
+            print ""
+            print "Goodbye cowardly, %s." % hero_name
+            # break statement will end the loop immediately
+            break
+        else: 
+            # User entered something we didn't offer
+            print "Thou fool. Thou has stubmledith (invalid input)."
 
-    print "Fight the goblin"
+fight()
